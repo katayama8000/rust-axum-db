7d4474f05f02bbf6ea15d774fc29ed2f26220b2b
katayama800 <tattu.0310@gmail.com>
2024-02-21 09:52:32 +0000

    add axum

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
-use sqlx::{mysql::MySqlPoolOptions, Row};
+use axum::{extract::State, routing::get, Router};
+use sqlx::{mysql::MySqlPoolOptions, Pool, Row};
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    // get from .env
-    // let url = format!(
-    //     "mysql://{}:{}@mariadb/{}",
-    //     std::env::var("MYSQL_USER").unwrap(),
-    //     std::env::var("MYSQL_PASSWORD").unwrap(),
-    //     std::env::var("MYSQL_DATABASE").unwrap()
-    // );
-    let vars = std::env::vars().collect::<Vec<_>>();
-    for (key, value) in vars {
-        println!("{}: {}", key, value);
-    }
+#[derive(Clone)]
+struct AppState {
+    pool: Pool<sqlx::MySql>,
+}
+
+fn router() -> Router<AppState> {
+    Router::new().route("/", get(handle_get))
+}
+
+async fn connect() -> Result<Pool<sqlx::MySql>, sqlx::Error> {
     let pool = MySqlPoolOptions::new()
         .max_connections(5)
         .connect("mysql://myuser:mypassword@mariadb/mydatabase")
         .await?;
+    Ok(pool)
+}
 
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = connect().await.expect("connect to database");
+    let state = AppState { pool };
+    let app = router().with_state(state);
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+    Ok(())
+}
+
+async fn handle_get(State(state): State<AppState>) {
+    println!("GET /");
     // insert some data
     sqlx::query("INSERT INTO mytable (name) VALUES (?)")
         .bind("World")
-        .execute(&pool)
-        .await?;
+        .execute(&state.pool)
+        .await
+        .unwrap();
     // fetch all
     let rows = sqlx::query("SELECT * FROM mytable")
-        .fetch_all(&pool)
-        .await?;
+        .fetch_all(&state.pool)
+        .await
+        .unwrap();
     println!("Got {} rows", rows.len());
     // print all
     for row in rows {
@@ -34,5 +51,4 @@ async fn main() -> Result<(), sqlx::Error> {
         let name: String = row.get("name");
         println!("id: {}, name: {}", id, name);
     }
-    Ok(())
 }
