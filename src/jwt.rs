b328e92a17f2cb3b4c12a8133918ccbcaf9674e9
katayama800 <tattu.0310@gmail.com>
2024-02-25 07:19:17 +0000

    minorfix

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,8 +1,10 @@
-use axum::http::{header::AUTHORIZATION, HeaderMap};
-use jsonwebtoken::{DecodingKey, EncodingKey, TokenData};
-use serde::{de::DeserializeOwned, Deserialize, Serialize};
-
 use crate::handler::User;
+use axum::http::{header::AUTHORIZATION, HeaderMap};
+use jsonwebtoken::{
+    decode, encode, errors::Error as JwtError, Algorithm, DecodingKey, EncodingKey, Header,
+    TokenData, Validation,
+};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 // should be in .env
 pub const JWT_SECRET_KEY: &str = "app-secret";
@@ -14,35 +16,16 @@ pub trait ClaimsGenerator<T> {
     fn generate_claims(_: &T) -> Self;
 }
 
-// encode token
-pub trait JwtEncoder {
-    fn encode<T: Serialize>(claims: T) -> String {
-        let mut header = jsonwebtoken::Header::default();
-        header.typ = Some(String::from("JWT"));
-        header.alg = jsonwebtoken::Algorithm::HS256;
-        // make token with claims and header
-        jsonwebtoken::encode(
-            &header,
-            &claims,
-            &EncodingKey::from_secret(JWT_SECRET_KEY.as_ref()),
-        )
-        .unwrap()
-    }
-}
-
 // decode token
-pub trait JwtDecoder<T: DeserializeOwned, E, R> {
-    fn parse_header(request: &R) -> Result<String, E>;
+pub trait JwtDecoder<T: DeserializeOwned> {
+    fn parse_header(request: &HeaderMap) -> Result<String, String>;
     // check token and decode
-    fn decode(&self, token: &str) -> Result<TokenData<T>, jsonwebtoken::errors::Error> {
-        match jsonwebtoken::decode::<T>(
+    fn decode(&self, token: &str) -> Result<TokenData<T>, JwtError> {
+        decode::<T>(
             token,
             &DecodingKey::from_secret(JWT_SECRET_KEY.as_ref()),
-            &jsonwebtoken::Validation::default(),
-        ) {
-            Ok(token_data) => Ok(token_data),
-            Err(e) => Err(e),
-        }
+            &Validation::default(),
+        )
     }
 }
 
@@ -67,15 +50,17 @@ impl ClaimsGenerator<User> for ApiClaims {
     }
 }
 
-#[derive(Default)]
 pub struct ApiJwt;
+
 impl ApiJwt {
     pub fn encode<T: Serialize>(claims: T) -> String {
-        let mut header = jsonwebtoken::Header::default();
-        header.typ = Some(String::from("JWT"));
-        header.alg = jsonwebtoken::Algorithm::HS256;
-        // make token with claims and header
-        jsonwebtoken::encode(
+        let header = Header {
+            typ: Some("JWT".into()),
+            alg: Algorithm::HS256,
+            ..Default::default()
+        };
+
+        encode(
             &header,
             &claims,
             &EncodingKey::from_secret(JWT_SECRET_KEY.as_ref()),
@@ -84,24 +69,20 @@ impl ApiJwt {
     }
 }
 
-impl JwtDecoder<ApiClaims, String, HeaderMap> for ApiJwt {
+impl JwtDecoder<ApiClaims> for ApiJwt {
     fn parse_header(header: &HeaderMap) -> Result<String, String> {
-        let header_value = header.get(AUTHORIZATION);
-
-        let token = header_value.unwrap();
-        let mut split_token = token.to_str().unwrap().split_whitespace();
-        match split_token.next() {
-            Some(schema_type) => {
-                if schema_type != "Bearer" {
-                    return Err("Invalid schema type".to_string());
+        match header.get(AUTHORIZATION) {
+            Some(token) => {
+                let mut split_token = token.to_str().unwrap().split_whitespace();
+                match split_token.next() {
+                    Some(schema_type) if schema_type == "Bearer" => match split_token.next() {
+                        Some(jwt_token) => Ok(jwt_token.to_string()),
+                        None => Err("No JWT token found".to_string()),
+                    },
+                    Some(_) | None => Err("Invalid schema type".to_string()),
                 }
             }
-            None => return Err("No schema type found".to_string()),
-        };
-
-        match split_token.next() {
-            Some(jwt_token) => Ok(jwt_token.to_string()),
-            None => Err("No JWT token found".to_string()),
+            None => Err("No Authorization header found".to_string()),
         }
     }
 }
